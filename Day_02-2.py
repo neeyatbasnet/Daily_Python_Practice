f4891221ab431d03aa6cc0648a6089915117fa40
Neeyat Basnet <neeyatbasnet01@gmail.com>
2026-02-10 03:16:08 +0530

    Basic loop use

---
+++ Day_02-2.py
@@ -0,0 +1,6 @@
+"""The provided code stub reads an integer,n, from STDIN .
+For all non-negative integers i<n , print i square."""
+
+a=int(input("Enter a number"))
+for i in range (a):
+    print(i*i)
