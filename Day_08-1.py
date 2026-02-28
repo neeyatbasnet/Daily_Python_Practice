42a1d57f8acaa6c6ee9e51c3e6203199fa3609b8
Neeyat Basnet <neeyatbasnet01@gmail.com>
2026-02-28 19:16:42 +0530

    generator object + any()

---
+++ Day_08-1.py
@@ -0,0 +1,15 @@
+"""In the first line, print True if S has any alphanumeric characters. Otherwise, print False.
+In the second line, print True if S has any alphabetical characters. Otherwise, print False.
+In the third line, print True if S has any digits. Otherwise, print False.
+In the fourth line, print True if S has any lowercase characters. Otherwise, print False.
+In the fifth line, print True if S has any uppercase characters. Otherwise, print False."""
+
+#any(iterable) is a built in python function which checks if any element in the iterable is True.
+
+if __name__ == '__main__':
+    s = input("Enter a string:")
+    print(any(c.isalnum() for c in s))
+    print(any(c.isalpha() for c in s))
+    print(any(c.isdigit() for c in s))
+    print(any(c.islower() for c in s))
+    print(any(c.isupper() for c in s)) 
